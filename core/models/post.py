53d2c3bdf14b55f5fb42f6c5d13bbaca5c3558be
Doonu <DonuSuzerain@yandex.ru>
2024-10-27 17:29:39 +0300

    build: создана новая таблица профиля

--- core/models/post.py
+++ core/models/post.py
@@ -1,17 +1,14 @@
-from typing import TYPE_CHECKING
-
-from sqlalchemy import String, Text, ForeignKey
-
+from sqlalchemy import String, Text
 from .base import Base
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-
-if TYPE_CHECKING:
-    from .user import User
+from sqlalchemy.orm import Mapped, mapped_column
+from .mixins import UserRelationMixin
 
 
-class Post(Base):
+class Post(UserRelationMixin, Base):
+    _user_back_populates = "posts"
+
     title: Mapped[str] = mapped_column(String(100), unique=False)
     body: Mapped[str] = mapped_column(Text, default="", server_default="")
 
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
-    user: Mapped["User"] = relationship(back_populates="posts")
+    # user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    # user: Mapped["User"] = relationship(back_populates="posts")
---
+++ core/models/profile.py
@@ -0,0 +1,14 @@
+from .base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+from typing import Optional
+
+from .mixins import UserRelationMixin
+
+
+class Profile(UserRelationMixin, Base):
+    _user_back_populates = "profile"
+    _user_id_unique = True
+
+    first_name: Mapped[Optional[str]] = mapped_column(String(40))
+    last_name: Mapped[Optional[str]] = mapped_column(String(40))
