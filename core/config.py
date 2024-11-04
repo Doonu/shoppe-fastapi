d4f19f3fbd1b85daea66d5afa00718fc2caa5a96
Doonu <DonuSuzerain@yandex.ru>
2024-11-04 13:49:30 +0300

    feat: добавлены env переменные

--- core/config.py
+++ core/config.py
@@ -1,9 +1,16 @@
+import os
+from dotenv import load_dotenv
+
 from pydantic_settings import BaseSettings
 from pydantic import BaseModel
 
+env_file = ".env.dev" if os.getenv("ENV") == "dev" else ".env.prod"
+load_dotenv(env_file)
+
 
 class DbSetting(BaseModel):
-    url: str = "postgresql+asyncpg://postgres:qwerty@localhost:5436/"
+    print(os.getenv("DATABASE_URL"))
+    url: str = os.getenv("DATABASE_URL")
     echo: bool = True
 
 
