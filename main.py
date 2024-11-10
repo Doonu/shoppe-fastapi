320c071de3bc55b147c4b7302c81742845a80f3c
Doonu <DonuSuzerain@yandex.ru>
2024-11-10 16:24:13 +0300

    feat: добавлена обработка cors

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
 from contextlib import asynccontextmanager
 
 import uvicorn
 
+from fastapi.middleware.cors import CORSMiddleware
 from core.config import settings
 from fastapi import FastAPI
 from api_v1 import router as router_v1
@@ -12,7 +14,21 @@ async def lifespan(app: FastAPI):
     yield
 
 
+if os.getenv("ENV") == "dev":
+    origins = ["http://45.10.43.121:3000"]
+else:
+    origins = ["*"]
+
+
 app = FastAPI(title="Shop app", lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 
 
