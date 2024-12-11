a8d9aadb3fca58f427f8d51c0dcb52595a934be1
Doonu <DonuSuzerain@yandex.ru>
2024-12-11 21:34:39 +0300

    feat: добавлена сущность order

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,4 +1,13 @@
-__all__ = ("Base", "Product", "DatabaseHelper", "db_helper", "User", "Post", "Profile")
+__all__ = (
+    "Base",
+    "Product",
+    "DatabaseHelper",
+    "db_helper",
+    "User",
+    "Post",
+    "Profile",
+    "Order",
+)
 
 from .base import Base
 from .product import Product
@@ -6,3 +15,4 @@ from .db_helper import db_helper, DatabaseHelper
 from .user import User
 from .post import Post
 from .profile import Profile
+from .order import Order
