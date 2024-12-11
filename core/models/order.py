eb4452b587a729af7c809887d4862c36f6a3559b
Doonu <DonuSuzerain@yandex.ru>
2024-12-11 22:13:06 +0300

    feat: добавлены связи для заказа и для продуктов

--- core/models/order.py
+++ core/models/order.py
@@ -1,13 +1,23 @@
 from datetime import datetime
 
 from sqlalchemy import func, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+from .order_product import Order_product_association
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .product import Product
 
 
 class Order(Base):
     promocode: Mapped[str] = mapped_column(String, nullable=True)
     created_at: Mapped[datetime] = mapped_column(
         server_default=func.now(), default=datetime.now
+    )
+
+    products: Mapped[list["Product"]] = relationship(
+        secondary=Order_product_association, back_populates="order"
     )
--- core/models/product.py
+++ core/models/product.py
@@ -1,9 +1,18 @@
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, relationship
 
 from .base import Base
+from .order_product import Order_product_association
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .order import Order
 
 
 class Product(Base):
     name: Mapped[str]
     price: Mapped[int]
     description: Mapped[str]
+
+    orders: Mapped[list["Order"]] = relationship(
+        secondary=Order_product_association, back_populates="product"
+    )
